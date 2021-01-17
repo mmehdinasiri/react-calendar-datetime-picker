e69bffaac1f14973e3c184d78d8b125d018beed4
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-01-17 23:51:13 +0330

    add quick start page contetn

--- example/src/Views/QuickStart.tsx
+++ example/src/Views/QuickStart.tsx
@@ -1,10 +1,62 @@
 import React from 'react'
 import { DocLayout } from '../Component'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import {
+  // dark,
+  tomorrowNightEighties
+  // atomOneDarkReasonable
+} from 'react-syntax-highlighter/dist/esm/styles/hljs'
+
+const usageString = `
+import { DtPicker } from 'react-datetime-picker'
+import 'react-datetime-picker/dist/index.css'
+
+const DatePicker = () => {
+  const [date, setDate] = useState(null)
+  return (
+      <DtPicker
+        onChange={setDateEn}
+        defaultValue={dateEn}
+        type='single'
+        local='en'
+        withTime
+        showWeekend
+      />
+  )
+}
+export default DatePicker
+`
 
 const QuickStart = () => {
   return (
     <DocLayout>
-      <h1>QuickStart</h1>
+      <section className='my-10'>
+        <h1 className='text-3xl my-3'>Installation:</h1>
+        <p className='text-base mb-1'>The package can be installed via NPM:</p>
+        <div className='w-2/3'>
+          <SyntaxHighlighter style={tomorrowNightEighties}>
+            {'npm install react-datetime-picker'}
+          </SyntaxHighlighter>
+        </div>
+        <p className='text-base mb-1'>Or by using Yarn:</p>
+        <div className='w-2/3'>
+          <SyntaxHighlighter style={tomorrowNightEighties}>
+            {'yarn add react-datetime-picker'}
+          </SyntaxHighlighter>
+        </div>
+      </section>
+      <section className='my-10'>
+        <h1 className='text-3xl my-3'>Usage:</h1>
+        <p className='text-base mb-1'>Here's an example of basic usage:</p>
+        <div className='w-2/3'>
+          <SyntaxHighlighter
+            style={tomorrowNightEighties}
+            language='javascript '
+          >
+            {usageString}
+          </SyntaxHighlighter>
+        </div>
+      </section>
     </DocLayout>
   )
 }
