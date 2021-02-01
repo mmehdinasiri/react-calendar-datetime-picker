68b9ef8c5e5d5e89ca29112be77fb2f769b1338f
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-02-02 00:57:55 +0330

    v1.2.3

--- website/src/Component/Layout/Header.tsx
+++ website/src/Component/Layout/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
           <span className='text-xl text-text-lightest font-medium'>
             <Link to='/'>
               React date-time picker{' '}
-              <span className='text-xs ml-2'>v1.2.2</span>
+              <span className='text-xs ml-2'>v1.2.3</span>
             </Link>
           </span>
         </div>
