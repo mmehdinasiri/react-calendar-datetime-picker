96087374431a4d30938b36f3de51a035a12de91b
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-02-04 21:44:42 +0330

    fix home page

--- website/src/Views/Home.tsx
+++ website/src/Views/Home.tsx
@@ -7,10 +7,10 @@ const Home = () => {
     <Layout>
       <div className='mx-auto text-center '>
         <h1 className='text-4xl text-primary font-extrabold mb-2'>
-          React Date-Time Picker
+          React Calendar Date-Time Picker
         </h1>
         <h2 className='text-lg text-primary font-medium '>
-          The Simple and fast English and Persian calender for React
+          The simple and fast Gregorian and Jalali calender for react
         </h2>
         <div className='mx-auto w-60 my-4 my-6'>
           <img
@@ -20,7 +20,7 @@ const Home = () => {
         </div>
         <div>
           <Link to='/docs/quick-start' className='btn mr-2 font-bold'>
-            Get Start
+            Get Started
           </Link>
           <a
             className='btn font-bold'
@@ -34,26 +34,26 @@ const Home = () => {
           <h3 className='text-3xl text-primary font-bold  mb-4'>Features</h3>
           <ul className='mx-auto table list-disc text-text'>
             <li className='text-left'>
-              Support English and Persian(Jalali) calender
+              Supports Gregorian and Jalali calender
             </li>
-            <li className='text-left'>Use context api to share data</li>
+            <li className='text-left'>Uses context api to share data</li>
             <li className='text-left'>
-              Support Three type of calender: single day - range dates - multi
+              Supports three types of calender: single day - date range - multi
               dates
             </li>
             <li className='text-left'>Fully customizable</li>
-            <li className='text-left'>Support maximum and minimum dates </li>
+            <li className='text-left'>Supports maximum and minimum dates </li>
             <li className='text-left'>
-              Capability to add a list of disable dates
+              Capability to add a list of disabled dates
             </li>
             <li className='text-left'>
-              Support Time for single and range type
+              Supports time for single and range type
             </li>
+            <li className='text-left'>Capability to mark weekends</li>
             <li className='text-left'>
-              Capability to show days of weekend and today button
+              Function called for change, open and close events
             </li>
-            <li className='text-left'>Api for open and close events</li>
-            <li className='text-left'>Support Tyepscript</li>
+            <li className='text-left'>Supports Typescript</li>
           </ul>
         </div>
       </div>
