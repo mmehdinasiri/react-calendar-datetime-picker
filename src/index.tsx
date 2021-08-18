d7163ef596547ff6b1d4d4bcda4d8b61c5e8294a
mehdinasiri <m.mehdi.nasiri@gmail.com>
2021-08-18 20:22:27 +0430

    fix exprting types

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import { convertToEn, convertToFa } from './Utilities'
 import DtCalendar from './DtCalendar'
 import DtPicker from './DtPicker'
+interface IDay {
+  year: number
+  month: number
+  day: number
+  fullDay?: string
+  hour?: number
+  minute?: number
+}
 
+interface IRange {
+  from: Day
+  to: Day
+}
 type Day = IDay | null
 type Range = IRange | null
 type Multi = IDay[] | null
