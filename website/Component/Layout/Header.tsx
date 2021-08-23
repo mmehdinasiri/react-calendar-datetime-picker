926fc60c4c4cb0b69a1b983926d92cb5321834df
mehdinasiri <m.mehdi.nasiri@gmail.com>
2021-08-24 01:38:59 +0430

    v1.5.5

--- website/Component/Layout/Header.tsx
+++ website/Component/Layout/Header.tsx
@@ -15,7 +15,7 @@ const Header = () => {
 						<Link href='/'>
 							<a className='cursor-pointer'>
 								React calendar date-time picker{' '}
-								<span className='text-xs ml-1'>v1.5.4</span>
+								<span className='text-xs ml-1'>v1.5.5</span>
 							</a>
 						</Link>
 					</span>
